46fc9122333524c46f1913dd2cb841cb4a76e7ab
venkat1210 <ramanasuja12@gmail.com>
2022-11-01 11:50:30 +0530

    whiteboard

---
+++ ui/whiteboard-collab/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Container from './components/container/container';
+
+function App() {
+  return (
+ 
+   
+   <Container/>
+   
+  );
+}
+
+
+export default App;
